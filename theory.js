ac274b2437f19e0b632345fe1de5915bfbaf8f02
MihaiGolub <mihai.golubov@gmail.com>
2023-02-11 23:02:05 +0200

    js theory 1 hour

---
+++ theory.js
@@ -0,0 +1,25 @@
+/*
+3 + 2
+*/
+/*const Person = {
+    Mihai: "human",
+    Age: 26,
+    Height: 170,
+}
+console.log(Person.Age)*/
+/*console.log(Person);*/
+/*console.log(Person.Age);*/
+//console.dir(console);
+//console.table(console);
+/*
+Denumirea Variabilelor
+PascaleCase - Fiecare cuvint cu litera majuscula, spre ex "MihailGolubov, ButonExpediere etc" se folosesc in denumirea tipurilor si claseror.
+MAJOR - Toate literele cu majuscula, cu lunie jos intre cuvinte, se folosesc in constante care sunt cunoscute inaintea executarii aplicatiei.Spre ex "USER_PASSWORD, USER_ID"
+camelCase - se incepe cu minuscula si restul cuvintelor se incep cu majuscula. Spre ex"culoareAutoVinzare, ageUserToday etc."
+
+
+
+*/
+/*let myName = 'Mihai'
+console.log(myName)
+*/
